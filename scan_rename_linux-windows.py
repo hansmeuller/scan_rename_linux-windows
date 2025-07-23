1a6086d800334c0a10e50af9d8e1abea73a94e89
hansmeuller <118677299+hansmeuller@users.noreply.github.com>
2025-07-23 20:45:58 +0200

    uodate

--- scan_rename_linux-windows.py
+++ scan_rename_linux-windows.py
@@ -17,7 +17,7 @@ scans_folder = "ftp://fritz.box/PortableSSD/scans/_test"
 def extract_text_with_ocr(pdf_path):
     try:
         logger.info(f"Starte OCR für Datei: {pdf_path}")
-        # Konvertiere PDF zu Bilder
+        # Konvertiere in Bilder
         pages = convert_from_path(pdf_path)
         text = ""
         for page in pages:
