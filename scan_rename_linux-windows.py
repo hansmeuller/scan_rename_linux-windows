4b4c2fb57dc291994435cbbdabe099cbcc435e11
hansmeuller <118677299+hansmeuller@users.noreply.github.com>
2025-08-03 07:37:39 +0200

    up

--- scan_rename_linux-windows.py
+++ scan_rename_linux-windows.py
@@ -10,7 +10,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger()
 
-# Pfad Scans
+# Pfad
 scans_folder = "ftp://fritz.box/PortableSSD/scans/_test"
 
 # OCR
